ac102e3b30dfe6553879d40c9cf055f412d0b86e
sweyome <sweyome1@gmail.com>
2019-08-27 20:00:54 -0400

    MVP and stretch

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -1,12 +1,47 @@
 const  db = require('../data/db-config')
 
-module.exports ={
+module.exports = {
     find,
-    findById
+    findById,
+    findSteps,
+    add,
+    update,
+    remove,
+    addStep
 }
+
 function find(){
-return db('Schemes')
+    return db('schemes')
 }
-function findById(id){
-    return db('Schemes').where({id}).first();
+
+function findById(id) {
+    return db('schemes')
+        .where({ id });
 }
+
+function findSteps(id) {
+    return db('steps')
+        .where({ scheme_id: id });
+}
+
+function add({ scheme_name }) {
+    return db('schemes').insert({ scheme_name });
+}
+
+function update({ scheme_name }, id) {
+    return db('schemes')
+        .where({ id })
+        .update({ scheme_name })
+}
+
+function remove(id) {
+    return db('schemes')
+        .where({ id })
+        .del()
+}
+function addStep({ instructions, step_number }, id) {
+    return db('steps')
+        .insert({ instructions, step_number, scheme_id: id })
+}
+
+
